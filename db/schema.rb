37557d8e1a3764236a8e9bd54105ea63adeb1ad2
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2025-07-10 02:09:56 +0000

    fix rubocop errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,48 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_10_005510) do
+ActiveRecord::Schema[8.0].define(version: 20_250_710_005_510) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension 'pg_catalog.plpgsql'
 
-  create_table "budget_categories", force: :cascade do |t|
-    t.string "name"
-    t.decimal "spending_limit_percentage"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_budget_categories_on_deleted_at"
+  create_table 'budget_categories', force: :cascade do |t|
+    t.string 'name'
+    t.decimal 'spending_limit_percentage'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.datetime 'deleted_at'
+    t.index ['deleted_at'], name: 'index_budget_categories_on_deleted_at'
   end
 
-  create_table "budget_cycles", force: :cascade do |t|
-    t.string "name", null: false
-    t.decimal "total_budget", precision: 10, scale: 2, null: false
-    t.date "start_date", null: false
-    t.date "end_date", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_budget_cycles_on_deleted_at"
+  create_table 'budget_cycles', force: :cascade do |t|
+    t.string 'name', null: false
+    t.decimal 'total_budget', precision: 10, scale: 2, null: false
+    t.date 'start_date', null: false
+    t.date 'end_date', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.datetime 'deleted_at'
+    t.index ['deleted_at'], name: 'index_budget_cycles_on_deleted_at'
   end
 
-  create_table "budgets", force: :cascade do |t|
-    t.string "title", null: false
-    t.decimal "total_amount", precision: 10, scale: 2, null: false
-    t.bigint "budget_category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "budget_cycle_id", null: false
-    t.datetime "deleted_at"
-    t.index ["budget_category_id"], name: "index_budgets_on_budget_category_id"
-    t.index ["budget_cycle_id"], name: "index_budgets_on_budget_cycle_id"
-    t.index ["deleted_at"], name: "index_budgets_on_deleted_at"
+  create_table 'budgets', force: :cascade do |t|
+    t.string 'title', null: false
+    t.decimal 'total_amount', precision: 10, scale: 2, null: false
+    t.bigint 'budget_category_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'budget_cycle_id', null: false
+    t.datetime 'deleted_at'
+    t.index ['budget_category_id'], name: 'index_budgets_on_budget_category_id'
+    t.index ['budget_cycle_id'], name: 'index_budgets_on_budget_cycle_id'
+    t.index ['deleted_at'], name: 'index_budgets_on_deleted_at'
   end
 
-  create_table "dashboards", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'dashboards', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "budgets", "budget_categories"
-  add_foreign_key "budgets", "budget_cycles"
+  add_foreign_key 'budgets', 'budget_categories'
+  add_foreign_key 'budgets', 'budget_cycles'
 end
